ebf58bd06d199eb6f25682bed18045af8f3506a9
Roy Ivy III <rivy.dev@gmail.com>
2020-09-19 10:48:09 -0500

    Add "binutils" into any Cygwin installation
    - adds some useful/needed compiler-related utilities
      - the GNU assembler (`...-as`), which is required by some opam packages
      - `strip`

--- lib/install-ocaml-windows.cmd
+++ lib/install-ocaml-windows.cmd
@@ -6,10 +6,14 @@ set variant=%5
 
 set CYGWIN_ROOT=c:\cygwin
 <<<<<<< HEAD
+<<<<<<< HEAD
 %2\setup-x86_64.exe --quiet-mode --root %CYGWIN_ROOT% --site http://cygwin.mirror.constant.com --packages curl,diff,diffutils,git,m4,make,patch,perl,rsync,mingw64-x86_64-gcc-core,mingw64-x86_64-gcc-g++,unzip
 copy %2\setup-x86_64.exe %CYGWIN_ROOT%
 =======
 %toolPath%\setup-x86_64.exe --quiet-mode --root %CYGWIN_ROOT% --site http://cygwin.mirror.constant.com --packages curl,diff,diffutils,git,m4,make,patch,perl,rsync,unzip,mingw64-i686-gcc-core,mingw64-x86_64-gcc-core
+=======
+%toolPath%\setup-x86_64.exe --quiet-mode --root %CYGWIN_ROOT% --site http://cygwin.mirror.constant.com --packages curl,diff,diffutils,git,m4,make,patch,perl,rsync,unzip,mingw64-i686-gcc-core,mingw64-x86_64-gcc-core,mingw64-i686-binutils,mingw64-x86_64-binutils
+>>>>>>> 062c152 (Add "binutils" into any Cygwin installation)
 copy %toolPath%\setup-x86_64.exe %CYGWIN_ROOT%
 >>>>>>> 2cdfd3c (Add support for OCaml compiler version variants)
 set PATH=%CYGWIN_ROOT%\wrapperbin;%CYGWIN_ROOT%\bin;%PATH%
