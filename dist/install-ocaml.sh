b59b3761f8075e9c313a2968f60588424e42716b
David Allsopp <david.allsopp@metastack.com>
2021-02-17 12:35:55 +0000

    Build OCaml from sources on Windows too

--- dist/install-ocaml.sh
+++ dist/install-ocaml.sh
@@ -5,7 +5,7 @@ set -eu
 COMPILER="$1"
 
 if [[ $(opam config var os) = 'win32' ]]; then
-  COMPILER="ocaml-variants.${COMPILER}+mingw64c"
+  COMPILER="ocaml-variants.${COMPILER}+mingw64"
 fi
 
 opam switch set build 2>/dev/null || opam switch create build "${COMPILER}"
