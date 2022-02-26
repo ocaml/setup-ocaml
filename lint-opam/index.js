501895d9d7b08e514e092ed48e0f45959e14c0cc
Sora Morimoto <sora@morimoto.io>
2022-02-26 11:32:28 +0900

    Update default runtime to node16
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- lint-opam/index.js
+++ lint-opam/index.js
@@ -2996,56 +2996,15 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 
 function opamLint() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, (0,exec.exec)("opam", ["lint"])];
-                case 1:
-                    _a.sent();
-                    return [2 /*return*/];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        yield (0,exec.exec)("opam", ["lint"]);
     });
 }
 function opamDuneLint() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, (0,exec.exec)("opam", ["exec", "--", "opam-dune-lint"])];
-                case 1:
-                    _a.sent();
-                    return [2 /*return*/];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        yield (0,exec.exec)("opam", ["exec", "--", "opam-dune-lint"]);
     });
 }
 
@@ -3059,69 +3018,26 @@ var opam_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arg
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var opam_generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 
 
 function installOpamPackages() {
-    return opam_awaiter(this, void 0, void 0, function () {
-        return opam_generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    core.startGroup("Install opam packages");
-                    return [4 /*yield*/, (0,exec.exec)("opam", [
-                            "install",
-                            ".",
-                            "--deps-only",
-                            "--with-test",
-                            "--with-doc",
-                        ])];
-                case 1:
-                    _a.sent();
-                    core.endGroup();
-                    return [2 /*return*/];
-            }
-        });
+    return opam_awaiter(this, void 0, void 0, function* () {
+        core.startGroup("Install opam packages");
+        yield (0,exec.exec)("opam", [
+            "install",
+            ".",
+            "--deps-only",
+            "--with-test",
+            "--with-doc",
+        ]);
+        core.endGroup();
     });
 }
 function installOpamDuneLint() {
-    return opam_awaiter(this, void 0, void 0, function () {
-        return opam_generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    core.startGroup("Install opam-dune-lint");
-                    return [4 /*yield*/, (0,exec.exec)("opam", ["depext", "--install", "opam-dune-lint"])];
-                case 1:
-                    _a.sent();
-                    core.endGroup();
-                    return [2 /*return*/];
-            }
-        });
+    return opam_awaiter(this, void 0, void 0, function* () {
+        core.startGroup("Install opam-dune-lint");
+        yield (0,exec.exec)("opam", ["depext", "--install", "opam-dune-lint"]);
+        core.endGroup();
     });
 }
 
@@ -3135,65 +3051,22 @@ var lint_opam_awaiter = (undefined && undefined.__awaiter) || function (thisArg,
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var lint_opam_generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 
 
 
 function run() {
-    return lint_opam_awaiter(this, void 0, void 0, function () {
-        var error_1;
-        return lint_opam_generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 5, , 6]);
-                    return [4 /*yield*/, installOpamPackages()];
-                case 1:
-                    _a.sent();
-                    return [4 /*yield*/, installOpamDuneLint()];
-                case 2:
-                    _a.sent();
-                    return [4 /*yield*/, opamLint()];
-                case 3:
-                    _a.sent();
-                    return [4 /*yield*/, opamDuneLint()];
-                case 4:
-                    _a.sent();
-                    return [3 /*break*/, 6];
-                case 5:
-                    error_1 = _a.sent();
-                    if (error_1 instanceof Error) {
-                        core.setFailed(error_1.message);
-                    }
-                    return [3 /*break*/, 6];
-                case 6: return [2 /*return*/];
+    return lint_opam_awaiter(this, void 0, void 0, function* () {
+        try {
+            yield installOpamPackages();
+            yield installOpamDuneLint();
+            yield opamLint();
+            yield opamDuneLint();
+        }
+        catch (error) {
+            if (error instanceof Error) {
+                core.setFailed(error.message);
             }
-        });
+        }
     });
 }
 run();
