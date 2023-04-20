dcc1dce8c30a25a664c8383b69261768afe3cc8e
Sora Morimoto <sora@morimoto.io>
2023-04-21 00:11:14 +0900

    Update npm deps
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- lint-fmt/index.js
+++ lint-fmt/index.js
@@ -4820,10 +4820,10 @@ var store = __nccwpck_require__(9557);
 (module.exports = function (key, value) {
   return store[key] || (store[key] = value !== undefined ? value : {});
 })('versions', []).push({
-  version: '3.29.1',
+  version: '3.30.1',
   mode: IS_PURE ? 'pure' : 'global',
   copyright: '© 2014-2023 Denis Pushkarev (zloirock.ru)',
-  license: 'https://github.com/zloirock/core-js/blob/v3.29.1/LICENSE',
+  license: 'https://github.com/zloirock/core-js/blob/v3.30.1/LICENSE',
   source: 'https://github.com/zloirock/core-js'
 });
 
