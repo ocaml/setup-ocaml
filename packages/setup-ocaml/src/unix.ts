4470014c495e61ebc73ebc8cd4c1c77a624172cd
Sora Morimoto <sora@morimoto.io>
2024-12-24 14:25:30 +0900

    Revert "Run `opam update --depext` before installing system dependencies"

--- packages/setup-ocaml/src/unix.ts
+++ packages/setup-ocaml/src/unix.ts
@@ -56,3 +56,14 @@ export async function installUnixSystemPackages() {
     }
   }
 }
+
+export async function updateUnixPackageIndexFiles() {
+  const isGitHubRunner = process.env.GITHUB_ACTIONS === "true";
+  if (isGitHubRunner) {
+    if (PLATFORM === "linux") {
+      await exec("sudo", ["apt-get", "update"]);
+    } else if (PLATFORM === "macos") {
+      await exec("brew", ["update"]);
+    }
+  }
+}
